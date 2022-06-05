99deb55919125b187c1c68ef0ae50baf2f044870
dragan992 <dragan_blok1@hotmail.com>
2022-06-05 22:37:08 +0200

    first practice after JS basics course

---
+++ index.js
@@ -0,0 +1,35 @@
+//Recap kod Srkija 5.6.2022
+
+function vocke(arrTemplateArgument) {
+    arrTemplateArgument();
+  }
+  
+  function maloSimple(br) {
+    console.log(br);
+  }
+  // vocke();
+  let cvarak = 'cvarak';
+  maloSimple(cvarak);
+  
+  let arr = (prviArgument) => {
+    console.log(prviArgument)
+  }
+  
+  
+  vocke(arr);
+  
+  console.log(arr);
+  arr('hej')
+  
+  // {
+  //   let re = 'baba'
+  // }
+  // console.log(re);
+  
+  const pederAray = [];
+  pederAray.push
+  console.log(pederAray);
+  
+  peder = "Gacha";
+  
+  var bb = 
